6f4cb84a3aed7bb6059ca4527911ad5e74dcd57e
Oleksii Horbachevskyi <ohorbachevskyi@softjourn.com>
2017-11-15 17:36:53 +0200

    #1 remove unnecessary db configurations from general config

--- config.js
+++ config.js
@@ -2,13 +2,5 @@
 
 module.exports = {
     env: process.env.ENV || 'development',
-    port: process.env.PORT || 3000,
-    db: {
-        dialect: 'mysql',
-        host: '127.0.0.1',
-        port: '',
-        name: 'foundation',
-        user: 'root',
-        password: ''
-    }
+    port: process.env.PORT || 3000
 }
