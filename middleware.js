5ef80f6967c7aaedbfd6188ba9ce00a13578aee2
Oleksii Horbachevskyi <ohorbachevskyi@softjourn.com>
2017-11-16 13:04:21 +0200

    handle response statused

--- middleware.js
+++ middleware.js
@@ -12,9 +12,14 @@ const initRoute = (server, resources, parent) => {
             let handlerMethod = resource.endpoints[endpoint];
             let parts = endpoint.split(' ');
 
-            server[parts[0]](parent + resource.name + parts[1], function(req, res, next) {
-                let result = handlers[resource.name][handlerMethod](req.params);
-                res.send(result);
+            server[parts[0]](parent + resource.name + parts[1], async (req, res, next) => {
+                try {
+                    let result = await handlers[resource.name][handlerMethod](req.params);
+                    res.send(result ? 200 : 404, result || 'Resource not found');
+                } catch (err) {
+                    logger.error(err);
+                    res.send(500, 'Oops! Something went wrong..');
+                }
                 next();
             });
 
