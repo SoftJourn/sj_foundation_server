cdb1cf091b2c07e4d186a1c0b0df96b8a165c5a4
Taras Khoma <tkhoma@softjourn.com>
2019-05-14 13:17:22 +0300

    add promise to auth

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,31 +1,24 @@
 var ldap = require('ldapjs');
 
-exports.login = (params) => {
-    function authDN(dn, password, cb) {
+function ldapAuth(username, password) {
+    var dn = 'uid=' + username + ',ou=People,ou=Users,dc=ldap,dc=sjua';
+    return new Promise((resolve, reject) => {
         var client = ldap.createClient({
             url: 'ldap://ldap.softjourn.if.ua:389'
         });
 
-        client.bind(dn, password, function(err) {
+        client.bind(dn, password, (err) => {
             client.unbind();
-            cb(err === null, err);
+            if (err === null) {
+                resolve({ success : true, username: username });
+                return;
+            }
+
+            reject({ notAllowed: true });
         });
-    }
+    });
+}
 
-    function output(res, err) {
-        if (res) {
-            console.log('success');
-        } else {
-            console.log(err);
-        }
-    }
-
-    var dn = 'uid=' + params.username + ',ou=People,ou=Users,dc=ldap,dc=sjua';
-    authDN(dn, params.password, output);
-
-    if (params.username === 'user' && params.password === 'user') {
-        return { success : true, username: params.username };
-    }
-
-    throw { notAllowed: true };
+exports.login = (params) => {
+    return ldapAuth(params.username, params.password);
 };
